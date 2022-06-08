a7b02231fca4e828a5ad80c2abf84d44e31aa5e6
Gustavo-F <gusta.ff230@mail.com>
2022-06-08 00:41:10 -0300

    .

--- API/src/index.js
+++ API/src/index.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 
 import usuarioController from './controller/usuarioController.js'; 
- 
+import animeRepository from './controller/animeController'
 
 import express from 'express' 
 import cors from 'cors';
@@ -12,6 +12,7 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+server.use(animeRepository);
 server.use(usuarioController);
 
 server.listen(process.env.PORT, () => console.log(`API conectada na porta ${process.env.PORT}`))
